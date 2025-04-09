f36f42b4268f948352ee61a071e4a8b83f567844
Timothy Romero <the_flu@yahoo.com>
2025-04-08 20:05:34 -0500

    updates

--- getAnimals.php
+++ getAnimals.php
@@ -1,3 +1,36 @@
 <?php
-
+$data = '{   "animals":
+            [
+                {
+                    "id":0,
+                    "name":"Bob",
+                    "species":"chicken",
+                    "sex":"male",
+                    "breed":"barred rock",
+                    "age":12,
+                    "photo":"./images/barredRooster.jpg"
+                },
+                {
+                    "id":1,
+                    "name":"Henrietta",
+                    "species":"chicken",
+                    "sex":"female",
+                    "breed":"barred rock",
+                    "age":10,
+                    "photo":"./images/Bob.jpg"
+                },
+                {
+                    "id":2,
+                    "name":"Hank",
+                    "species":"chicken",
+                    "sex":"male",
+                    "breed":"red",
+                    "age":5,
+                    "photo":"./images/red.jpg"
+                }
+            ]
+        }';
+	header('Access-Control-Allow-Origin: *');
+	header('Content-Type: application/json; charset=utf-8');
+	echo json_encode($data);
 ?>
